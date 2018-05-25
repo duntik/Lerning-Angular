80cfdd25a279fdfeb203d69f65cb4d7d91aa2c58
Ilja <duntiknews@yandex.ru>
2018-05-25 23:39:50 +0100

    working with components html and css

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  template: '<H2>Single car</H2>',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
