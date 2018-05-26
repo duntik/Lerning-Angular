3b80bf1be0ff7b86f10e2d73f4817eeb414079d3
Ilja <duntiknews@yandex.ru>
2018-05-26 01:43:29 +0100

    Testing bootstrap and meterialize css

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-car',
-  template: '<H2>Single car</H2>',
+  templateUrl: './car.component.html',
   styleUrls: ['./car.component.css']
 })
 export class CarComponent implements OnInit {
