5251989270774b44fc7b4f3db8959f509770f8c6
Ilja <duntiknews@yandex.ru>
2018-05-27 02:22:15 +0100

    Lerning Pipes

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -10,6 +10,12 @@ export class CarsComponent {
   carName = '';
   addCarStatus = false;
   cars = ['Ford', 'Audi', 'BMW','Mazda','Lada','Bently'];
+  dates = [
+    new Date(2015,3,4).toDateString(),
+    new Date(2016,5,2).toDateString(),
+    new Date(2019,7,4).toDateString(),
+    new Date(2014,4,6).toDateString(),
+  ];
 
   constructor(){
   }
@@ -19,10 +25,4 @@ export class CarsComponent {
     this.cars.push(this.carName);
     this.carName = '';
   }
-
-  setBigCarText(car: string){
-  return car.length > 4 ? true : false; 
-  }
-
-
 }
